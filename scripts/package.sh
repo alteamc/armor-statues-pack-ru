5fda97ef387e363a72ef45b417232f092a38afbd
dreamscached <dreamscache.d@gmail.com>
2021-07-29 21:05:31 +0400

    Исправления игнорируемых find файлов; исправление .gitlab-ci.yml

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-./scripts/format.sh
-
 mkdir -p build/datapacks
 find . -type f ! \(                                                                                                    \
   -path ./.git/\* -o                                                                                                   \
-  -path ./reference/\* -o                                                                                              \
-  -path ./Makefile -o                                                                                                  \
   -path ./.gitignore -o                                                                                                \
   -path ./.gitlab-ci.yml -o                                                                                            \
-  -path ./VERSION -o                                                                                                   \
   -path ./data/armor_statues/loot_tables/book/\*                                                                       \
+  -path ./scripts/\*                                                                                                   \
+  -path ./Makefile -o                                                                                                  \
+  -path ./VERSION -o                                                                                                   \
 \) | zip "build/datapacks/Armor Statues $(cat VERSION).zip" -@
