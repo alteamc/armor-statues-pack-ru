be9ae4abcc0da2d08121d36ae76861a4a5827c77
dreamscached <dreamscache.d@gmail.com>
2021-12-10 16:16:11 +0100

    Исправление синтаксиса

--- scripts/package.sh
+++ scripts/package.sh
@@ -7,5 +7,5 @@ find . -type f ! \(                                                             
   -path ./data/armor_statues/loot_tables/book/\*                                                                       \
   -path ./scripts/\*                                                                                                   \
   -path ./Makefile -o                                                                                                  \
-  -path ./VERSION -o                                                                                                   \
+  -path ./VERSION                                                                                                      \
 \) | zip "build/datapacks/Armor Statues $(cat VERSION).zip" -@
