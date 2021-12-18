2f974953663a486fad68f17ff17af99483d2827f
dreamscached <dreamscache.d@gmail.com>
2021-12-18 20:56:43 +0100

    Fixes

--- scripts/compile_book.sh
+++ scripts/compile_book.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 printf "%s" "Compiling book.json... "
 
 booktmp="$(mktemp)"
--- scripts/package.sh
+++ scripts/package.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+set -x
 mkdir -p build/datapacks
-find . -type f ! \(                                                                                                    \
+find . -type f -not \(                                                                                                 \
   -path ./.git/\* -o                                                                                                   \
   -path ./.gitignore -o                                                                                                \
-  -path ./.gitlab-ci.yml -o                                                                                            \
-  -path ./data/armor_statues/loot_tables/book/\*                                                                       \
-  -path ./scripts/\*                                                                                                   \
+  -path ./.github/\* -o                                                                                                \
+  -path ./build/\* -o                                                                                                  \
+  -path ./data/armor_statues/loot_tables/book/\* -o                                                                    \
+  -path ./scripts/\* -o                                                                                                \
   -path ./Makefile -o                                                                                                  \
   -path ./VERSION                                                                                                      \
 \) | zip "build/datapacks/Armor Statues $(cat VERSION).zip" -@
